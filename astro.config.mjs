1be335ec4107c4f7129befcdfc4809530447410a
Alan Monroy Bernedo <alan.monroy@ucsp.edu.pe>
2026-07-23 12:23:25 -0500

    add SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://septrumperu.com",
+	integrations: [
+		sitemap({
+			filter: (page) => !page.includes("/terminos") && !page.includes("/privacidad"),
+		}),
+	],
 	vite: {
 		plugins: [tailwindcss()],
 	},
